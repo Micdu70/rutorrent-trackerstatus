7c11be7e3a5100b0f552e7cf60345ce49a949a5b
Micdu70 <Micdu70@users.noreply.github.com>
2019-03-10 11:32:25 +0100

    Update init.js

--- init.js
+++ init.js
@@ -1,20 +1,21 @@
 plugin.loadLang();
 
-plugin.onLangLoaded = function() {
-	if (plugin.canChangeColumns()) {
-		plugin.config = theWebUI.config;
-		theWebUI.config = function(data) {
-			theWebUI.tables.trt.columns.push({text: theUILang.Tracker_status, width: "200px", id: "msg", type: TYPE_STRING});
-			plugin.config.call(this, data);
-		}
+if (plugin.canChangeColumns()) {
+	plugin.config = theWebUI.config;
+	theWebUI.config = function(data) {
+		this.tables.trt.columns.push({text: 'Tracker - Status', width: '200px', id: 'msg', type: TYPE_STRING});
+		plugin.config.call(this, data);
+		plugin.trtRenameColumn();
+        }
+
+	plugin.trtRenameColumn = function() {
+		if(plugin.allStuffLoaded)
+			theWebUI.getTable("trt").renameColumnById("msg",theUILang.Tracker_status);
+		else
+			setTimeout(arguments.callee,1000);
 	}
 }
 
 plugin.onRemove = function() {
 	theWebUI.getTable("trt").removeColumnById("msg");
-}
-
-plugin.langLoaded = function() {
-	if(plugin.enabled)
-		plugin.onLangLoaded();
 }
